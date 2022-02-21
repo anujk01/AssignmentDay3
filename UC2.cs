83e908f94d59e333799d02687ab3dab38bdf02db
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-21 18:56:02 +0530

    DailyEmpWage

---
+++ UC2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentDay1
+{
+    class UC2
+    {
+        public static void EmpDailyWage()
+        {
+                int empRatePerHr = 20;
+                int empHrs = 8;
+                int salary = (empRatePerHr * empHrs);
+                Console.WriteLine("Employee Daily Wage is " + salary);
+ 
+        }
+    }
+}
